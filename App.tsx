d473bd9248749993838e3b0945addbf210ec6ff0
Pedro Dias <pdr050203@gmail.com>
2024-12-03 17:47:09 -0300

    docs: add font to project

--- App.tsx
+++ App.tsx
@@ -1,10 +1,17 @@
 import { ThemeProvider } from 'styled-components'
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 import theme from './src/theme';
 
 import { SignIn } from './src/screens/SignIn';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold});
+
+  if(!fontsLoaded) {
+    return <></>
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <SignIn />
