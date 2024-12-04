aa7fab2f55c34b1440df3a1b24a7516e6bb785df
Pedro Dias <pdr050203@gmail.com>
2024-12-03 23:36:56 -0300

    feat: add status bar styles

--- App.tsx
+++ App.tsx
@@ -5,16 +5,18 @@ import theme from './src/theme';
 
 import { SignIn } from './src/screens/SignIn';
 import { Loading } from './src/components/Loading';
+import { StatusBar } from 'react-native';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold});
 
-  if(fontsLoaded) {
+  if(!fontsLoaded) {
     return <Loading />
   }
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent/>
       <SignIn />
     </ThemeProvider>
   );
