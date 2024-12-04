c8aee02875f8a0c548748ac80f421c83fd5240a3
Pedro Dias <pdr050203@gmail.com>
2024-12-04 00:01:12 -0300

    feat: add button component

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,6 +1,7 @@
 import { Container, Title, Slogan } from './styles';
 
 import backgroundImg from '../../assets/background.png'
+import { Button } from '../../components/Button';
 
 export function SignIn() {
   return (
@@ -9,6 +10,8 @@ export function SignIn() {
       <Slogan>
         Gestão de uso de veículos
       </Slogan>
+
+      <Button title='Entrar com Google' />
     </Container>
   );
 }
